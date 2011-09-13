045f084a36a728e6ec29f526e41054a8a5585dec
Facundo Batista <facundo@taniquetil.com.ar>
2011-09-13 18:03:50 -0300

    Included logo and how to distribute it.

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ class CustomInstall(install):
         """Run parent install, and then save the install dir in the script."""
         install.run(self)
 
+        # fix installation path in the script(s)
         for script in self.distribution.scripts:
             script_path = os.path.join(self.install_scripts,
                                        os.path.basename(script))
@@ -52,6 +53,19 @@ class CustomInstall(install):
             with open(script_path, 'wb') as fh:
                 fh.write(content)
 
+        # fix the icon path, and save the .desktop file where it should be
+        src_desktop = self.distribution.get_name() + '.desktop'
+        if not os.path.exists(self._custom_apps_dir):
+            os.makedirs(self._custom_apps_dir)
+        dst_desktop = os.path.join(self._custom_apps_dir, src_desktop)
+
+        with open(src_desktop, 'rb') as fh:
+            content = fh.read()
+        icon = os.path.join(self._custom_data_dir,
+                            'encuentro', 'logos', 'icon-32.png')
+        content = content.replace('@ INSTALLED_ICON @', icon)
+        with open(dst_desktop, 'wb') as fh:
+            fh.write(content)
 
     def finalize_options(self):
         """Alter the installation path."""
@@ -60,6 +74,7 @@ class CustomInstall(install):
         # the data path is under 'prefix'
         data_dir = os.path.join(self.prefix, "share",
                                 self.distribution.get_name())
+        apps_dir = os.path.join(self.prefix, "share", "applications")
 
         # if we have 'root', put the building path also under it (used normally
         # by pbuilder)
@@ -67,12 +82,14 @@ class CustomInstall(install):
             build_dir = data_dir
         else:
             build_dir = os.path.join(self.root, data_dir[1:])
+            apps_dir = os.path.join(self.root, apps_dir[1:])
 
         # change the lib install directory so all package files go inside here
         self.install_lib = build_dir
 
         # save this custom data dir to later change the scripts
         self._custom_data_dir = data_dir
+        self._custom_apps_dir = apps_dir
 
 
 setup(
@@ -87,7 +104,7 @@ setup(
     url = 'https://launchpad.net/encuentro',
 
     packages = ["encuentro"],
-    package_data = {"encuentro": ["ui/*.glade"]},
+    package_data = {"encuentro": ["ui/*.glade", "logos/icon-32.png"]},
     scripts = ["bin/encuentro"],
 
     cmdclass = {
