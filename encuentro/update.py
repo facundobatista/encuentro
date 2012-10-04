30be2d1632edd8b7a3d56c3ced8ee824ed6c12de
Facundo Batista <facundo@taniquetil.com.ar>
2012-10-04 00:10:07 -0300

    Use different backends and merge.

--- encuentro/update.py
+++ encuentro/update.py
@@ -29,7 +29,7 @@ from twisted.internet import defer
 from twisted.web import client
 
 
-EPISODES_URL = "http://www.taniquetil.com.ar/encuentro-v02.bz2"
+BACKENDS_URL = "http://www.taniquetil.com.ar/backends-v01.list"
 BASEDIR = os.path.dirname(__file__)
 
 logger = logging.getLogger('encuentro.update')
@@ -70,7 +70,8 @@ class UpdateUI(object):
             x, y = parent_pos
             self.dialog.move(x + 50, y + 50)
 
-        self._update(self.tview_insert)
+        self._update(lambda text:
+                     self.tview_insert(" ".join(map(str, text)) + '\n'))
         self.main.main_window.set_sensitive(False)
         self.dialog.run()
         self.textview.get_buffer().set_text("")
@@ -94,45 +95,97 @@ class UpdateUI(object):
         """Update the content from server."""
         self.closed = False
 
-        logger.info("Updating episodes metadata")
-        tell_user("Descargando la lista de episodios...\n")
+        logger.info("Downloading backend list")
+        tell_user("Descargando la lista de backends...")
         try:
-            compressed = yield client.getPage(EPISODES_URL)
+            backends_file = yield client.getPage(BACKENDS_URL)
         except Exception, e:
-            logger.error("Problem when downloading episodes: %s", e)
-            tell_user("Hubo un PROBLEMA al bajar los episodios: " + str(e))
+            logger.error("Problem when downloading backends: %s", e)
+            tell_user("Hubo un PROBLEMA al bajar la lista de backends:", e)
             return
         if self.closed:
             return
+        backends_list = [l.strip().split() for l in backends_file.split("\n")
+                         if l and l[0] != '#']
+        print "======= bajado backends", repr(backends_list)
 
-        tell_user("Descomprimiendo el archivo....\n")
-        new_content = bz2.decompress(compressed)
-        logger.debug("Downloaded data decompressed ok")
+        backends = {}
+        for b_name, b_url in backends_list:
+            logger.info("Downloading backend metadata for %r", b_name)
+            tell_user("Descargando la lista de episodios para backend %s...",
+                      b_name)
+            try:
+                compressed = yield client.getPage(b_url)
+            except Exception, e:
+                logger.error("Problem when downloading episodes: %s", e)
+                tell_user("Hubo un PROBLEMA al bajar los episodios: ", e)
+                return
+            if self.closed:
+                return
 
-        tell_user("Actualizando los datos internos....\n")
-        new_data = json.loads(new_content)
+            tell_user("Descomprimiendo el archivo....")
+            new_content = bz2.decompress(compressed)
+            logger.debug("Downloaded data decompressed ok")
+            backends[b_name] = json.loads(new_content)
+
+        tell_user("Conciliando datos de diferentes backends")
+        logger.debug("Merging backends data")
+        new_data = self._merge(backends)
+
+        tell_user("Actualizando los datos internos....")
         logger.debug("Updating internal metadata (%d)", len(new_data))
         self.main.merge_episode_data(new_data)
 
-        tell_user(u"¡Todo terminado bien!\n")
+        tell_user(u"¡Todo terminado bien!")
         self.on_dialog_destroy(None)
 
-# FIXME: que el método "_update":
-#   - baje primero algo que le indique qué bajar
-#   - que baje todo lo que tiene que bajar
-#   - que mergee
-#   - que loguee resultados del merge
-#
-# Reglas del merge:
-# - description: si son iguales, usa uno; si uno es None, usa el otro; si no
-#   los concatena (el más corto de ambos primero)
-# - url: quedarse con la de 'conect', que es más detallada
-# - channel: quedarse con la de 'conect'
-# - title: quedarse con la de 'conect'
-# - section: quedarse con la de 'conect'
-# - duration: si son distintos, tomar el que no es None
-#
-# Loguear:
-# - cuantos "pisados" (con el mismo episode_id)
-# - cuantos con channel que no correspondian
-# - cuantos con title que no correspondian
+    def _merge(self, backends):
+        """Merge content from all backends.
+
+        This is for v01, with only 'encuentro' and 'conectar' data.
+        """
+        enc_data = dict((x['episode_id'], x) for x in backends['encuentro'])
+        con_data = dict((x['episode_id'], x) for x in backends['conectar'])
+        common = set(enc_data) & set(con_data)
+        logger.debug("Merging: encuentro=%d conectar=%d (common=%d)",
+                     len(enc_data), len(con_data), len(common))
+
+        # what is in not common in both goes untouched
+        final_data = ([enc_data[epid] for epid in set(enc_data) - common] +
+                      [con_data[epid] for epid in set(con_data) - common])
+
+        # what is common, we need to do the merge
+        for epid in common:
+            enc_ep = enc_data[epid]
+            con_ep = con_data[epid]
+
+            enc_desc = enc_ep['description']
+            con_desc = con_ep['description']
+            if enc_desc == con_desc:
+                description = enc_desc
+            elif enc_desc is None:
+                description = con_desc
+            elif con_desc is None:
+                description = enc_desc
+            else:
+                # not None, or they would have been the same, let's concat
+                # both, shorter first
+                if len(con_desc) < len(enc_desc):
+                    description = con_desc + ' ' + enc_desc
+                else:
+                    description = enc_desc + ' ' + con_desc
+
+            # if both are equal (None or not), it also works
+            if enc_ep['duration'] is None:
+                duration = con_ep['duration']
+            else:
+                duration = enc_ep['duration']
+
+            d = dict(episode_id=epid, description=description,
+                     duration=duration, url=con_ep['url'],
+                     channel=con_ep['channel'], title=con_ep['title'],
+                     section=con_ep['section'])
+            final_data.append(d)
+
+        logger.debug("Merged, final: %d", len(final_data))
+        return final_data
