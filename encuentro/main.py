9da926ac78334ece5cf1da519c2069176b7bec41
Facundo Batista <facundo@taniquetil.com.ar>
2013-03-28 08:31:48 -0300

    Refactored the imports.

--- encuentro/main.py
+++ encuentro/main.py
@@ -18,30 +18,26 @@
 
 import sys
 
+from encuentro import logger
+
 # we put here EpisodeData only for legacy reasons: unpickle of old pickles
 # will try to load EpisodeData from this namespace
 from encuentro.data import EpisodeData
 
-verbose = len(sys.argv) > 1 and sys.argv[1] == '-v'
-
-from encuentro import logger, NiceImporter
-logger.set_up(verbose)
+import pynotify
 
 from PyQt4.QtGui import QApplication
 
 
-pynotify = None
-with NiceImporter('pynotify', 'python-notify', '0.1.1'):
-    import pynotify
-    pynotify.init("Encuentro")
-
-
-
 def start(version):
     """Rock and roll."""
+    pynotify.init("Encuentro")
+    verbose = len(sys.argv) > 1 and sys.argv[1] == '-v'
+    logger.set_up(verbose)
+
     # the order of the lines hereafter are very precise, don't mess with them
-    # FIXME: see if we can put NiceImporter here
     app = QApplication(sys.argv)
+    print "Qt Application started", app
     import qt4reactor
     qt4reactor.install()
     from encuentro.ui.main import MainUI
